fba058df0d6a17e3a73b2cdb8b1ff33364198503
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-04-25 01:39:52 +0200

    krsteni api

--- js/vremenskaPrognoza.js
+++ js/vremenskaPrognoza.js
@@ -1,16 +1,17 @@
+(()=>{
+
+const callApi=function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.842986&lon=20.953148&lang=sr&appid=23121e705933ec1c73e50fe68de58b25&units=metric'
 ).then(response=>response.json()).then(data=>{
- let temperatura=data['main']['temp'];
- let description=data['weather'][0]['description'];
- let wind=data['wind']['speed'];
- let humidity=data['main']['humidity'];
-temperature.textContent=`${temperatura} степени`;
-descr.textContent=`${description}`;
-windSpeed.textContent=`${wind}`;
-humid.textContent=`${humidity}`;
-});
+   weatherUI(data);
+});}
+
+ const weatherUI=function(data){
+document.getElementById('temperature').textContent=`${data['main']['temp']}`;
+document.getElementById('windSpeed').textContent=`${data['weather'][0]['description']}`;
+document.getElementById('descr').textContent=`${data['wind']['speed']}`;
+document.getElementById('humidity').textContent=`${data['main']['humidity']}`;
+ }
  
-const temperature= document.getElementById('temperature');
-const windSpeed=document.getElementById('windSpeed');
-const descr=document.getElementById('descr');
-const humid=document.getElementById('humidity');
+ weatherUI(callApi());
+})();
