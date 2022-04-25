0ea4898f6a0569ab0cbbb19fc9863bbd683ad58d
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-04-25 03:12:53 +0200

    a

--- js/vremenskaPrognoza.js
+++ js/vremenskaPrognoza.js
@@ -1,20 +1,19 @@
 (()=>{
-
+ 
+       
    function callApi() {
       fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.842986&lon=20.953148&lang=sr&appid=23121e705933ec1c73e50fe68de58b25&units=metric'
       ).then(response => response.json()).then(data => {
          weatherUI(data);
-      }).catch(function (err) {
-         console.log(err);
       });
    }
-
- function weatherUI(data) {
+   
+   function weatherUI(data) {
       document.getElementById('temperature').textContent = `${data['main']['temp']} степени целзијуса`;
       document.getElementById('windSpeed').textContent = `${data['wind']['speed']} метара у секунди`;
       document.getElementById('descr').textContent = `${data['weather'][0]['description']}`;
       document.getElementById('humidity').textContent = `${data['main']['humidity']}%`;
    }
- 
- weatherUI(callApi());
-})();
+    
+    callApi();
+   })();
