eac2e8bc806bcddfe97d731fcc7badc56164bde4
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-04-25 12:48:56 +0200

    a

--- js/vremenskaPrognoza.js
+++ js/vremenskaPrognoza.js
@@ -13,13 +13,27 @@
       document.getElementById('descr').textContent = `${data['current']['weather'][0]['description']}`;
       document.getElementById('humidity').textContent = `${data['current']['humidity']}%`;
       if(data['alerts'][0]['event']){
-         let pocetak=new Date((data['alerts'][0]['start'])*1000).toDateString();
-         let kraj=new Date((data['alerts'][0]['end'])*1000).toDateString();
-         document.getElementById('alerts').textContent = `${data['alerts'][0]['event']}`;
-         document.getElementById('alertsTime').textContent = `од ${pocetak} до ${kraj}`;
+        alerts(data);
 
       }
    }
-    
+    function alerts(data){
+       let event=data['alerts'][0]['event'];
+       let pocetak=new Date((data['alerts'][0]['start'])*1000).toDateString();
+       let kraj=new Date((data['alerts'][0]['end'])*1000).toDateString();
+
+       let currentWeather=document.getElementById('currentWeather');
+       currentWeather.innerHTML+=`
+       <tr class="redAlert"><th>Постоји упозорење</th>        <th>${event}</th>
+       </tr>
+       <tr>
+       <td> Од</td>
+       <td>${pocetak}</td>
+       </tr>
+       <tr>
+       <td>До</td>
+       <td>${kraj}</td>
+       <tr>`
+    }
     callApi();
    })();
