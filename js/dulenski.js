76f6e1014ba96711296395e2bd072b4a2c7eb7cc
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-05-09 00:11:33 +0200

    a

--- js/dulenski.js
+++ js/dulenski.js
@@ -1,26 +1,27 @@
 /*Prikazuje i sakriva navigaciju na telefonu*/
-const phoneNav=document.getElementById('phone-nav').addEventListener('click', ()=>{
-  const element = document.getElementById("meny");
-  if (element.style.display === "flex") {
-    element.style.display = "none";
-  } else {
-    element.style.display = "flex";
-  }
-}); 
-
-//ovo fraca flex, sprecava da bude neprikazan meni nakon promene velicine ekrana na desktopu
- window.addEventListener('resize', ()=>{
-  const element = document.getElementById("meny");
-  console.log(window.innerWidth);
-  console.log(' ');
-     if(window.innerWidth>650){
+const phoneNav = document
+  .getElementById("phone-nav")
+  .addEventListener("click", () => {
+    const element = document.getElementById("meny");
+    if (element.style.display === "flex") {
+      element.style.display = "none";
+    } else {
       element.style.display = "flex";
     }
- })
- 
-  /*Navigacija ide gore dole...*/
- let pastScroll = window.pageYOffset;
-window.onscroll = function() {
+  });
+
+//ovo fraca flex, sprecava da bude neprikazan meni nakon promene velicine ekrana na desktopu
+window.addEventListener("resize", () => {
+  const element = document.getElementById("meny");
+  console.log(window.innerWidth);
+  if (window.innerWidth > 650) {
+    element.style.display = "flex";
+  }
+});
+
+/*Navigacija ide gore dole...*/
+let pastScroll = window.pageYOffset;
+window.onscroll = function () {
   let newScroll = window.pageYOffset;
   if (pastScroll > newScroll) {
     document.getElementById("navbar").style.top = "0";
@@ -28,8 +29,8 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-200px";
   }
   pastScroll = newScroll;
-}
+};
 
 /* Za godinu u futeru */
-let year=new Date().getFullYear();
-document.getElementById('year').innerHTML=year;
+let year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
